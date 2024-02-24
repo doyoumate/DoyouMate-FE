0edcfeaf77f4dfffcc0acddc1c6688e6d5e1e705
earlgrey02 <san06036@naver.com>
2024-02-24 21:10:56 +0900

    Fix: Axios가 쿼리 파라미터를 비정상적으로 인코딩하던 문제 해결

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://localhost:8080',
+  paramsSerializer: params =>
+    Object.entries(params)
+      .map(
+        ([key, value]) =>
+          `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+      )
+      .join('&')
 })
 
 export default api
