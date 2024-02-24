ec65383d48e6d9b9f64ef9c8b2a5f153a3ec3e07
earlgrey02 <san06036@naver.com>
2024-02-24 21:10:38 +0900

    Feat: searchLectures에 페이지네이션 관련 파라미터 추가

--- src/module/lecture/api.ts
+++ src/module/lecture/api.ts
@@ -1,9 +1,17 @@
 import api from '../../lib/axios.ts'
 import { FilterResponse, Lecture, SearchLecturesRequest } from './lecture'
 
-const searchLectures = async (request: SearchLecturesRequest) => {
+const searchLectures = async (
+  request: SearchLecturesRequest,
+  page: number,
+  size: number
+) => {
   const response = await api.get<Lecture[]>(`/lecture`, {
-    params: request
+    params: {
+      ...request,
+      page: page,
+      size: size
+    }
   })
 
   return response.data
