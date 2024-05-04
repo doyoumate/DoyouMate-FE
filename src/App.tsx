145e3aead43fec1f9e5efcadf075cbff144b736d
earlgrey02 <san06036@naver.com>
2024-05-04 18:53:51 +0900

    Feat: App 컴포넌트 내에 AlertModal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native'
-import RootNavigator from './navigators/RootNavigator.tsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { useState } from 'react'
+import RootNavigator from './navigators/RootNavigator.tsx'
+import AlertModal from './components/AlertModal.tsx'
 
 const App = () => {
   const [queryClient] = useState(() => new QueryClient())
@@ -13,6 +14,7 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <NavigationContainer>
           <RootNavigator />
+          <AlertModal />
         </NavigationContainer>
       </QueryClientProvider>
     </Provider>
