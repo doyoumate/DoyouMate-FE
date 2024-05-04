af124459bce823dc8b5866ca959636ca0200f399
earlgrey02 <san06036@naver.com>
2024-05-04 18:36:43 +0900

    Chore: ESLint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'prettier'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    '@typescript-eslint/no-shadow': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'prettier/prettier': ['error', { printWidth: 120 }]
   }
 }
