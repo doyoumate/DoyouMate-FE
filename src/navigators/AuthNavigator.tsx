a03a1288226cd0744d3f8e66033d1f728d51dfb7
earlgrey02 <san06036@naver.com>
2024-05-04 18:54:11 +0900

    Feat: AuthNavigator 컴포넌트 구현

---
+++ src/navigators/AuthNavigator.tsx
@@ -0,0 +1,27 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigatorParamList } from './navigation'
+import LoginScreen from '../screens/auth/LoginScreen.tsx'
+import SignUpScreen from '../screens/auth/SignUpScreen.tsx'
+
+const Stack = createStackNavigator<NavigatorParamList>()
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="login"
+      screenOptions={{
+        headerLeftContainerStyle: { left: 10 },
+        headerStyle: { backgroundColor: 'rgb(250, 250, 250)' },
+        headerShadowVisible: false,
+        headerBackTitleVisible: false,
+        headerTintColor: 'rgb(50, 50, 50)',
+        headerTitle: () => <></>,
+        cardStyle: { backgroundColor: 'rgb(250, 250, 250)' }
+      }}>
+      <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="signUp" component={SignUpScreen} />
+    </Stack.Navigator>
+  )
+}
+
+export default AuthNavigator
