ba6fc245fb805fdd3c4b052c2fce6f915d0898e6
earlgrey02 <san06036@naver.com>
2024-05-04 18:47:12 +0900

    Feat: LectureItem 컴포넌트 구현

--- src/components/LectureItem.tsx
+++ src/components/LectureItem.tsx
@@ -1,83 +1,82 @@
-import { Lecture } from '../module/lecture/lecture'
-import { StyleSheet, Text, View, ViewToken } from 'react-native'
+import { LectureResponse } from '../module/lecture/dto/response'
+import { StyleSheet, Text, View } from 'react-native'
 import { FontAwesome5, Ionicons } from '../lib/icon.ts'
-import Animated, {
-  SharedValue,
-  useAnimatedStyle,
-  withTiming
-} from 'react-native-reanimated'
 
 interface Props {
-  lecture: Lecture
-  viewableItems: SharedValue<ViewToken[]>
+  lecture: LectureResponse
 }
 
-const LectureItem = ({ lecture, viewableItems }: Props) => {
-  const animatedStyle = useAnimatedStyle(() => {
-    const isVisible = Boolean(
-      viewableItems.value
-        .filter(item => item.isViewable)
-        .find(viewableItem => viewableItem.item.id === lecture.id)
-    )
-
-    return {
-      opacity: withTiming(isVisible ? 1 : 0, {
-        duration: 500
-      })
-    }
-  }, [])
-
+const LectureItem = ({ lecture }: Props) => {
   return (
-    <Animated.View style={[styles.container, animatedStyle]}>
-      <Text style={styles.year}>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 11,
+          color: 'rgb(50, 50, 50)',
+          fontFamily: 'NanumSquare_acL'
+        }}>
         {lecture.year} {'>'} {lecture.semester}
       </Text>
-      <Text style={styles.name}>{lecture.name}</Text>
-      <Text style={styles.major}>{lecture.major}</Text>
+      <Text
+        style={{
+          fontSize: 15,
+          fontFamily: 'NanumSquare_acB'
+        }}>
+        {lecture.name}
+      </Text>
+      <Text
+        style={{
+          fontSize: 12,
+          fontFamily: 'NanumSquare_acR'
+        }}>
+        {lecture.major}
+      </Text>
       {lecture.professor && (
-        <View style={styles.professorContainer}>
-          <FontAwesome5 name="user" />
-          <Text style={styles.professor}>{lecture.professor}</Text>
+        <View style={styles.professor}>
+          <FontAwesome5 name="user" size={11} />
+          <Text
+            style={{
+              fontSize: 11,
+              fontFamily: 'NanumSquare_acR'
+            }}>
+            {lecture.professor}
+          </Text>
         </View>
       )}
       {lecture.date && (
-        <View style={styles.dateContainer}>
-          <Ionicons name="time-outline" />
-          <Text style={styles.date}>{lecture.date}</Text>
+        <View style={styles.date}>
+          <Ionicons name="time-outline" size={11} />
+          <Text
+            style={{
+              fontSize: 11,
+              fontFamily: 'NanumSquare_acR'
+            }}>
+            {lecture.date}
+          </Text>
         </View>
       )}
-    </Animated.View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    gap: 3,
+    gap: 4,
     width: '100%',
-    marginBottom: 10,
     paddingVertical: 12,
     paddingHorizontal: 10,
-    borderRadius: 4,
+    borderRadius: 8,
     backgroundColor: 'rgb(250, 250, 250)',
-    shadowColor: 'rgb(200, 200, 200)',
-    shadowOffset: {
-      width: 0.3,
-      height: 0.3
-    },
+    shadowColor: 'rgb(230, 230, 230)',
     shadowOpacity: 0.4,
+    shadowOffset: {
+      width: 0,
+      height: 0.2
+    },
     shadowRadius: 3,
-    elevation: 5
+    elevation: 3
   },
-  year: {
-    fontSize: 12,
-    color: 'rgb(50, 50, 50)'
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: 'bold'
-  },
-  major: { fontSize: 12 },
-  professorContainer: {
+  professor: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 3,
@@ -85,17 +84,11 @@ const styles = StyleSheet.create({
     marginBottom: 2,
     color: 'rgb(50, 50, 50)'
   },
-  professor: {
-    fontSize: 12
-  },
-  dateContainer: {
+  date: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 3,
     color: 'rgb(50, 50, 50)'
-  },
-  date: {
-    fontSize: 12
   }
 })
 
